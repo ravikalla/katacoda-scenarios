73b8353a9c35cc01e3bf610d5b8c85cefb2635ae
Ravi Kalla <ravi2523096@gmail.com>
2019-01-04 01:14:20 -0500

    updated shell scripts

--- bdd-cucumber-spring/env-init.sh
+++ bdd-cucumber-spring/env-init.sh
@@ -1,7 +1,8 @@
+ssh root@host01 "mkdir /root/projects"
+ssh root@host01 "chmod 777 /root/projects"
+ssh root@host01 "cd /root/projects"
 ssh root@host01 "git clone https://github.com/ravikalla/bdd-cucumber-spring-katacoda.git"
 ssh root@host01 "yum install tree -y"
-ssh root@host01 "mkdir /root/projects/temp"
-ssh root@host01 "chmod 777 /root/projects/temp"
 
 ssh root@host01 "mkdir -p /root/projects/temp1"
 ssh root@host01 "chcon -R -t svirt_sandbox_file_t /root/projects/temp1"
