ada34c3bae128e799e2e6a89d707b34dd6ef7e70
Ravi Kalla <ravi2523096@gmail.com>
2019-01-06 23:45:20 -0500

    NGINX install

--- bdd-cucumber-spring/set-env.sh
+++ bdd-cucumber-spring/set-env.sh
@@ -2,4 +2,7 @@
 cd /root/projects/
 git clone https://github.com/ravikalla/bdd-cucumber-spring-katacoda.git
 cd /root/projects/bdd-cucumber-spring-katacoda/
+
+sudo apt update
+sudo apt --yes --force-yes install nginx
 #~/.launch.sh
