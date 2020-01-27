deca80708c8baf8f83efed327973b32bf65f1cab
Ravi Kalla <ravi2523096@gmail.com>
2020-01-27 18:11:04 -0500

    Updated startup scripts

--- devops/set-env.sh
+++ devops/set-env.sh
@@ -6,5 +6,20 @@ cd /root/projects/online-account/
 sudo apt update
 sudo apt --yes --force-yes install nginx
 
+# Jenkins Build
+cd /root/projects
+sudo chmod 777 /var/run/docker.sock
+mkdir -p /jenkins_bkp/jenkins_home
+chmod -R 777 /jenkins_bkp
+git clone https://github.com/ravikalla/online-account.git
+cd online-account
+git checkout master
+cp Dockerfile-Jenkins-Maven ../Dockerfile
+cd ..
+docker build -t ravikalla/jenkins-maven-docker:v0.1 .
+
+# Jenkins Run
+docker run --detach -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -p 9080:8080 -p 50000:50000 -v /jenkins_bkp/jenkins_home:/var/jenkins_home --name jenkinsfordevops ravikalla/jenkins-maven-docker:v0.1
+
 clear scr
 #~/.launch.sh
