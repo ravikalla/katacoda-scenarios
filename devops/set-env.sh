0a8c00b17d69b2eb3ebae312fdf0624627ddffc0
Ravi Kalla <ravi2523096@gmail.com>
2020-01-27 18:18:09 -0500

    Avoid general update (apt update)

--- devops/set-env.sh
+++ devops/set-env.sh
@@ -19,7 +19,7 @@ docker build -t ravikalla/jenkins-maven-docker:v0.1 .
 docker run --detach -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -p 9080:8080 -p 50000:50000 -v /jenkins_bkp/jenkins_home:/var/jenkins_home --name jenkinsfordevops ravikalla/jenkins-maven-docker:v0.1
 
 # Install NGINX
-sudo apt update
+#sudo apt update
 sudo apt --yes --force-yes install nginx
 
 clear scr
