3580cc21b06d15a0e6aff3e056a83bde2a91ef2b
Ravi Shankar Kalla <ravi523096@yahoo.com>
2020-02-16 14:30:47 -0500

    Created bridge between Sonar and Jenkins Dockers

--- devops/set-env.sh
+++ devops/set-env.sh
@@ -21,6 +21,10 @@ docker build -t ravikalla/jenkins-maven-docker:v0.1 .
 # Jenkins Run
 docker run --detach -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -p 9080:8080 -p 50000:50000 -v /jenkins_bkp/jenkins_home:/var/jenkins_home --name jenkinsfordevops ravikalla/jenkins-maven-docker:v0.1
 
+docker network create --driver bridge jenkinssonarbridge
+docker network connect jenkinssonarbridge jenkinsfordevops
+docker network connect jenkinssonarbridge sonarqube
+
 # Install NGINX
 #sudo apt update
 sudo apt --yes --force-yes install nginx
